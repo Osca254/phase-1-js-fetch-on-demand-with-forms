11ea83ea5015bcb6a81d6bdd53fe3a8a6baf0e02
moringa <oscarokumu47@gmail.com>
2022-08-25 10:59:28 +0300

    modify package-lock.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector('form');
 
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+  
+      console.log(input.value);
+  
+      fetch(`http://localhost:3000/movies ${input.value}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // LOG: (3) [{…}, {…}, {…}]
+        let tittle = document.querySelector('section#movieDetails h4')
+        let summary = document.querySelector('sectionmovieDetails p')
+        tittle.innerText= data.tittle;
+        summary.innerText = data.summary;
+      });
+      return data;
+    });
+}
+  document.addEventListener('DOMContentLoaded', init);
+
+
